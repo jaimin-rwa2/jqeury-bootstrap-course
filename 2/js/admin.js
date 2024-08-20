4a28a3e0302cb5aa8712ab77224fa973f8df013f
MrJemmy <jbpatel1411@gmail.com>
2024-08-20 18:59:18 +0530

    project

---
+++ 2/js/admin.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".group-item ul li a").click(function(){
+        $(this).parent("li").find(".dropdown-menu").slideToggle();
+        $(this).parent("li").nextAll("li").find(".dropdown-menu").slideUp();
+        $(this).parent("li").prevAll("li").find(".dropdown-menu").slideUp();
+    });
+    $("form i").click(function(){
+        $("input").slideToggle();
+    })
+    $(".toggle").click(function(){
+        $(".icon").slideToggle();
+    })
+  
+});
+$(document).ready(function(){
+    $(".header-left i").click(function(){
+      $("body").toggleClass("active");
+    });
+});
