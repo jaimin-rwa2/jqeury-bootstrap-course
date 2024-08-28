4a65eaf24b304894c888cab5730295d252b44f36
MrJemmy <jbpatel1411@gmail.com>
2024-08-28 18:49:12 +0530

    side bar done

--- 100-admin-panel-1/js/script.js
+++ 100-admin-panel-1/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    $(".header-left i").click(function(){
-      $("aside").toggleClass("aside-slide");
-    });
+  // for sidebar
+  $(".header-left i").click(function(){
+    $("aside").toggleClass("aside-slide");
+  });
+
+  $(".aside-bottom .dropdown-item > li a").click(function(){
+    console.log($(this).parent().parent().parent().find("li").children(".dropdown-menu").slideUp());
+    $(this).parent().find(".dropdown-menu").slideToggle();
+    // (this).parent(".dropdown-item li").find(".dropdown-menu").slideToggle();
+  })
 });
+
